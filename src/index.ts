bf1285af79a612f18e9943060763ae6a459ecd1c
Raghav <raghav.gate@gmail.com>
2024-07-26 21:03:50 -0700

    Adding Restaurant feature

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+
+import {v2 as cloudinary} from "cloudinary";
 
 //establish connection to database
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to Database")); // casting in typescript
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 //create express server and assign to variable app
 const app = express();
@@ -20,7 +29,12 @@ app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health ok!" });
 });
 
+
 app.use("/api/my/user", myUserRoute);
+
+
+//endpoint for restaurant
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -25,3 +25,27 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant Name is required"),
+  body("city").notEmpty().withMessage("city is required"),
+  body("country").notEmpty().withMessage("country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("estimated delivery time must be a positive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("menu item price is required and must be a positive number"),
+  handleValidationErrors,
+];
